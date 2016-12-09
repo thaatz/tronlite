fbca53298a9f0b2b88cef6c2e71cd9d88c73d4e8
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2016-12-08 21:50:40 -0500

    added ccleaner example. Changed delimiter in settings.ini

---
+++ ccsetup524/run.bat
@@ -0,0 +1,5 @@
+:: https://www.piriform.com/docs/ccleaner/advanced-usage/command-line-parameters
+cls
+@echo off
+pushd "%~dp0" 2>NUL
+ccleaner.exe /auto
--- run.bat
+++ run.bat
@@ -1,21 +1,21 @@
 @echo off
 pushd "%~dp0" 2>NUL
-call :read_settings settings.ini
+REM call :read_settings settings.ini
 
 title Buttfixer 3000
-for /f "eol=# delims== tokens=1" %%i in (%SETTINGSFILE%) do (
-	call "%%i"
+for /f "eol=; delims== tokens=1" %%i in (settings.ini) do (
+	echo "%%i"
 )
 pause
 
-:read_settings
-set SETTINGSFILE=%1
-if not exist %SETTINGSFILE% (
-	echo settings.ini does not exist
-	pause
-    exit
-)
-for /f "eol=# delims== tokens=1,2" %%i in (%SETTINGSFILE%) do (
-    set %%i=%%j
-)
-exit /b 0
+REM :read_settings
+REM set SETTINGSFILE=%1
+REM if not exist %SETTINGSFILE% (
+	REM echo settings.ini does not exist
+	REM pause
+    REM exit
+REM )
+REM for /f "eol=# delims== tokens=1,2" %%i in (%SETTINGSFILE%) do (
+    REM set %%i=%%j
+REM )
+REM exit /b 0
