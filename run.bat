dc13a7b81996cacd2a962d5263280cc294d34e7c
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2016-12-16 22:26:54 -0500

    added a check for administrator rights

--- run.bat
+++ run.bat
@@ -3,6 +3,21 @@ set tronlog=%userprofile%\desktop\tronlite.log
 pushd "%~dp0" 2>NUL
 REM call :read_settings settings.ini
 
+fsutil dirty query %systemdrive% >NUL 2>&1
+if /i not !ERRORLEVEL!==0 (
+	color cf
+	cls
+	echo.
+	echo  ERROR
+	echo.
+	echo  Looks like you are not running this as admin.
+	echo.
+	echo  Close this window and re-run as an Administrator.
+	echo.
+	pause
+	exit /b 1	)
+
+
 title Buttfixer 3000
 echo tronlite %date% at %time%>>"%tronlite%"
 for /f "eol=; delims== tokens=1" %%i in (settings.ini) do (
