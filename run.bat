2fecb477a6903e4729f9ed2c31929e5f2806621b
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2016-12-17 13:38:39 -0500

    added some basic logging

--- run.bat
+++ run.bat
@@ -1,13 +1,19 @@
 @echo off
+set tronlog=%userprofile%\desktop\tronlite.log
 pushd "%~dp0" 2>NUL
 REM call :read_settings settings.ini
 
 title Buttfixer 3000
+echo tronlite %date% at %time%>>"%tronlite%"
 for /f "eol=; delims== tokens=1" %%i in (settings.ini) do (
-	pushd "%~dp0" 2>NUL
 	:: we pushd every time to reset the current directory
+	pushd "%~dp0" 2>NUL
+	echo. >>"%tronlite%"
+	echo %time% running %%i>>"%tronlite%"
 	call "%%i"
 )
+echo tronlite finished on %date% at %time%>>"%tronlite%"
+echo. >>"%tronlite%"
 pause
 
 REM :read_settings
