0c106c8a09a801fd7b94693e0aa9d5792562f92d
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2017-02-02 12:19:16 -0500

    Added script counter. Made formatting changes to log file.

--- run.bat
+++ run.bat
@@ -26,9 +26,12 @@ if /i not %ERRORLEVEL%==0 (
 title Tronlite
 :main
 cls
+:: internal count of scripts for the session
+set /a cnt=0
 for /f "eol=; delims== tokens=1" %%i in (settings.ini) do (
 	:: list out what we find in the settings.ini file
 	echo "%%i"
+	set /a cnt+=1
 )
 echo.
 echo dont forget to double check settings.ini
@@ -38,9 +41,9 @@ echo  º                                Tronlite                                 
 echo  ºÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄº
 echo  º What should we do when we're done?                                        º
 echo  º                                                                           º
-echo  º [1] Restart (recommended)                                                 º
-echo  º [2] Shutdown                                                              º
-echo  º [0] Do nothing                                                            º
+echo  º 1. Restart (recommended)                                                  º
+echo  º 2. Shutdown                                                               º
+echo  º 0. Do nothing                                                             º
 echo  ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
 set /p exs=Make selection: 
 if /i "%exs%"=="1" goto :m2
@@ -52,20 +55,28 @@ goto :main
 pause
 
 :m2
+echo.
 if /i "%exs%"=="1" set ext=%systemroot%\system32\shutdown.exe /r /f
 if /i "%exs%"=="2" set ext=%systemroot%\system32\shutdown.exe /s /f
 if /i "%exs%"=="0" set ext=
 REM echo %ext%
 REM pause
 
-echo tronlite %date% at %time%>>"%tronlog%"
+echo tronlite started on %date% at %time%>>"%tronlog%"
+echo tronlite started on %date% at %time%
+setlocal enabledelayedexpansion
+set cnt2=0
+:: this enables us to count within the for loop
+:: http://stackoverflow.com/questions/7522740/counting-in-a-for-loop-using-windows-batch-script
 for /f "eol=; delims== tokens=1" %%i in (settings.ini) do (
 	:: we pushd every time to reset the current directory
 	pushd "%~dp0" 2>NUL
-	echo. >>"%tronlog%"
-	echo running %%i>>"%tronlog%"
+	REM echo. >>"%tronlog%"
+	echo ^(!cnt2!/%cnt%^) running %%i>>"%tronlog%"
+	echo ^(!cnt2!/%cnt%^) running %%i
 	call "%%i"
 )
+endlocal
 echo tronlite finished on %date% at %time%>>"%tronlog%"
 echo. >>"%tronlog%"
 %ext%
