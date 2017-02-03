0fb00a27326f939bde612fa4f3d39eb54822060c
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2017-02-02 20:10:23 -0500

    jrt.bat will now poll, wait, and then close the log, making the JRT part mostly automated now

--- prep/jrt.bat
+++ prep/jrt.bat
@@ -1,5 +1,26 @@
 pushd %~dp0 2>NUL
 REM jrt.exe
 :: on windows xp, we need to use start /wait for the script to wait
-start /wait jrt.exe
-REM pause
+jrt.exe
+:: http://stackoverflow.com/questions/8177695/how-to-wait-for-a-process-to-terminate-to-execute-another-process-in-batch-file
+:: http://stackoverflow.com/questions/162291/how-to-check-if-a-process-is-running-via-a-batch-script
+:loop
+tasklist /fi "WINDOWTITLE eq jrt - notepad" 2>nul | find /i "notepad" >nul
+if ERRORLEVEL 1 (
+	:: this is what happens when it is not running
+	REM cls
+	REM echo its not here yet
+	ping localhost -n 6 >nul
+	goto loop
+) else (
+	:: this is what happens when it is running
+	REM cls
+	goto continue
+)
+
+:continue
+:: this is what happens after it runs
+:: restore the registry key we exported at the begining
+echo.
+taskkill /im notepad.exe /fi "WINDOWTITLE eq jrt - notepad" >nul
+pause
