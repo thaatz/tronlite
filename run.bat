f11ddaf5d22beb84e01fa304a5cf6fde15e968c6
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2019-10-18 13:12:11 -0400

    addressed an issue where comments in loops would throw errors

--- run.bat
+++ run.bat
@@ -28,7 +28,7 @@ cls
 :: internal count of scripts for the session
 set /a cnt=0
 for /f "eol=; delims=" %%i in (settings.ini) do (
-	:: list out what we find in the settings.ini file
+	REM :: list out what we find in the settings.ini file
 	echo %%i
 	set /a cnt+=1
 )
@@ -69,7 +69,8 @@ set cnt2=0
 :: http://stackoverflow.com/questions/7522740/counting-in-a-for-loop-using-windows-batch-script
 REM for /f "eol=; tokens=1" %%i in (settings.ini) do (
 for /f "eol=; delims=" %%i in (settings.ini) do (
-	:: we pushd every time to reset the current directory
+	REM comments in this loop need to start with REM and not :: https://stackoverflow.com/questions/19843849/unexpected-the-system-cannot-find-the-drive-specified-in-batch-file
+	REM :: we pushd every time to reset the current directory
 	pushd "%~dp0" 2>NUL
 	title Tronlite
 	REM echo. >>"%tronlog%"
@@ -77,12 +78,12 @@ for /f "eol=; delims=" %%i in (settings.ini) do (
 	echo ^(!cnt2!/%cnt%^) running %%i>>"%tronlog%"
 	echo ^(!cnt2!/%cnt%^) running %%i
 	call %%i
-	:: you can use quotes around %%i and it would allow you to use path names with spaces, but it would disallow using arugments
-	:: we take the quotes out here to allow additional arguments with some of the modules
+	REM :: you can use quotes around %%i and it would allow you to use path names with spaces, but it would disallow using arugments
+	REM :: we take the quotes out here to allow additional arguments with some of the modules
 )
 endlocal
 echo tronlite finished on %date% at %time%>>"%tronlog%"
-echo. >>"%tronlog%"
+echo.>>"%tronlog%"
 %ext%
 REM pause
 
